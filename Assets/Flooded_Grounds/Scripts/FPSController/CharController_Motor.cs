33182c573684f32ba7e95416ec9ad1c870f90d2f
taku1109 <taku37817@gmail.com>
2021-06-19 23:02:41 +0900

    no message

---
+++ Assets/Flooded_Grounds/Scripts/FPSController/CharController_Motor.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharController_Motor : MonoBehaviour {
+
+	public float speed = 10.0f;
+	public float runSpeed = 10.0f;
+	public float sensitivity = 30.0f;
+	public float WaterHeight = 15.5f;
+	CharacterController character;
+	public GameObject cam;
+	public GameObject camera;
+	float rotX, rotY;
+	public bool webGLRightClickRotation = true;
+	float gravity = 9.8f;
+
+	void Start(){
+		//LockCursor ();
+		character = GetComponent<CharacterController> ();
+		if (Application.isEditor) {
+			webGLRightClickRotation = false;
+			sensitivity = sensitivity * 1.5f;
+		}
+	}
+
+
+	void CheckForWaterHeight(){
+		if (transform.position.y < WaterHeight) {
+			gravity = 0f;			
+		} else {
+			gravity = -9.8f;
+		}
+	}
+
+
+
+	void Update(){
+		rotX = Input.GetAxis ("Mouse X") * sensitivity;
+		rotY = Input.GetAxis ("Mouse Y") * sensitivity;
+
+		//rotX = Input.GetKey (KeyCode.Joystick1Button4);
+		//rotY = Input.GetKey (KeyCode.Joystick1Button5);
+
+		Vector3 Pos = camera.transform.position;
+		CheckForWaterHeight ();
+
+
+		if (webGLRightClickRotation) {
+			if (Input.GetKey (KeyCode.Mouse0)) {
+				CameraRotation (cam, rotX, rotY);
+			}
+		} else if (!webGLRightClickRotation) {
+			CameraRotation (cam, rotX, rotY);
+		}
+	}
+
+	void CameraRotation(GameObject cam, float rotX, float rotY){		
+		transform.Rotate (0, rotX * Time.deltaTime, 0);
+		cam.transform.Rotate (-rotY * Time.deltaTime, 0, 0);
+	}
+
+
+
+
+}
---
+++ p/propertyInt.cs
@@ -0,0 +1,6 @@
+﻿namespace p
+{
+    internal class propertyInt
+    {
+    }
+}
